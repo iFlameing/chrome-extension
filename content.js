243b0efa33931803bd78959b33d130203c954f14
iFlameing <ialokkumarsingh0@gmail.com>
2018-04-06 22:18:38 +0530

    This contains code regarding extension

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ let filenames=[
     "IMG-20180325-WA0002.jpg"
 
 ]
-
+// In array you can put multiple image and you can acess by math.random 
 let img=document.getElementsByTagName("img");
 for(let imgs of img){   
     let file="image/"+filenames[0];
